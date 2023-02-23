2ea87bbf0cef931e625a3b4f8bfbbb4c5a91d908
rob@rwmconsultancy.nl <rob@rwmconsultancy.nl>
2023-02-23 10:38:09 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import "./App.css";
+
+const initalState = { count: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + action.payload };
+    case "decrement":
+      return { count: state.count - action.payload };
+    case "reset":
+      return { count: 0 };
+    default:
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initalState);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Let's see how the reducer works in a simple counter example.
+        <h1>Count: {state.count}</h1>
+        <div>
+          <button onClick={() => dispatch({ type: "increment", payload: 2 })}>
+            +
+          </button>
+          <button onClick={() => dispatch({ type: "decrement", payload: 1 })}>
+            -
+          </button>
+          <br />
+          <button onClick={() => dispatch({ type: "reset" })}>reset</button>
+        </div>
       </header>
     </div>
   );
